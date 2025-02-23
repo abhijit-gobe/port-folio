15080d34dfd045e48453af717a6dd93634fdbe1c
Abhijit Gobe <abhijitgobe00@gmail.com>
2025-02-23 20:10:58 +0530

    portfolio

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Abhijit Gobe",
-  profile: '/profile.png',
+  profile: '/aboutPic.jpg',
   designation: "Web Developer",
   description: "My name is Abhijit Gobe. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'abhijitgobe00@gmail.com',
